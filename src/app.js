d61df14562d4c30e8f50dd66a549947640540576
Froris <gamefoxmail@gmail.com>
2020-01-05 16:24:50 +0200

    setting up server

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
-import MainPage from './components/mainPage';
+import MainPage from './components/MainPage';
 import configureStore from './store/configureStore';
 import './styles/styles.scss';
 
